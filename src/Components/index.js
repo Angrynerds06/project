67ff369911ba55a4cb44acc0054c323d9f50c0bb
Prashant <pr.prashant0786@gmail.com>
2023-09-10 11:50:35 +0530

    first commit

---
+++ src/Components/index.js
@@ -0,0 +1,10 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Wrapper from './Wrapper';
+import Poster from './Poster';
+import Loading from './Loading';
+import About from './About';
+import Prodedure from './Prodedure';
+import Contact from './Contact';
+
+export { Navbar, Footer , Wrapper , Poster , Loading , About , Prodedure, Contact};
